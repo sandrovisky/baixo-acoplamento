0183e52d66f009a6c7c6a035b17605bd53931240
Sandro Garcia <sndr95@hotmail.com>
2022-10-03 18:50:00 -0300

    init

---
+++ app/lib/core/routers/routers.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Routers {
+  static String get initialRoute => '/';
+  static final Map<String, Widget Function(BuildContext, dynamic)> routes = {
+    '/': (_, args) => Scaffold(
+          appBar: AppBar(title: Text("Roberta, amor da minha vida!")),
+        ),
+  };
+
+  static Route? generateRoutes(RouteSettings settings) {
+    final routerName = settings.name;
+    final routerArgs = settings.arguments;
+
+    final navigateTo = routes[routerName];
+
+    if (navigateTo == null) return null;
+
+    return MaterialPageRoute(
+        builder: (context) => navigateTo.call(context, routerArgs));
+  }
+}
